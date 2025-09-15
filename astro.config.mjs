c59df99800d42f786fc9134e36c2d830aeb1a198
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2025-09-15 09:41:39 -0500

    feat: version 0001

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
+
+// const deployEnv = process.env.DEVELOPMENT; // e.g. "vercel" | "node"
+// const deployEnv = import.meta.env.DEV; // e.g. "vercel" | "node"
+// console.log('deployEnv', deployEnv);
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-    },
+    plugins: [tailwindcss()],
+  },
 
   integrations: [vue()],
-  adapter: node({
-    mode: 'standalone',
-  }),
+  // adapter: import.meta.env.DEVELOPMENT ? node({ mode: 'standalone' }) : vercel(),
+  adapter: vercel(),
+  // adapter: node({ mode: 'standalone' }),
 });
