e11dd4f5d28480426fb3f6d460eaa4cc304d40d1
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2025-09-20 10:46:22 -0500

    feat: version 0001

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,5 +14,6 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: false,
-  }
+  },
+  // trustedOrigins: ['http://localhost:4321', 'http://192.168.68.64:4321']
 });
