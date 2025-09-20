316f2d976205f1aa4da23ae50c44a557c7ef9600
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2025-09-20 03:53:46 -0500

    feat: version 0001

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+export const auth = betterAuth({
+  database: new Pool({
+    host: import.meta.env.DATABASE_HOST,
+    port: import.meta.env.DATABASE_PORT,
+    database: import.meta.env.DATABASE_NAME,
+    user: import.meta.env.DATABASE_USER,
+    password: import.meta.env.DATABASE_PWD,
+    idleTimeoutMillis: 60 * 1000 * 5, // 5 minutes
+    connectionTimeoutMillis: 30 * 1000, // 30 seconds
+    max: 20, // 20 connections
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+  }
+});
