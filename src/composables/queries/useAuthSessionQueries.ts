bbd161bdd89579e5a2d17b725fe8cb5d0827635d
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2025-10-06 01:01:08 -0500

    feat: version 0001

--- src/composables/queries/useAuthSessionQueries.ts
+++ src/composables/queries/useAuthSessionQueries.ts
@@ -1,22 +1,23 @@
 import { authClient } from '@/lib/auth-client.ts';
-import { useMutation, useQuery, useQueryClient, isServer } from '@tanstack/vue-query';
+// import { useMutation, useQuery, isServer } from '@tanstack/vue-query';
+import { useMutation, useQuery } from '@tanstack/vue-query';
 import { experimental_createQueryPersister } from '@tanstack/query-persist-client-core'
 
 export function useAuthSessionQueries() {
-  const queryClient = useQueryClient();
   
   const { data: sessionData, isFetching: isFetchingSession } = useQuery({
     queryKey: ['auth-session'],
     queryFn: () => authClient.getSession().then(res => res.data),
     staleTime: Infinity,
-    persister: isServer
-      ? Promise.resolve
-      : experimental_createQueryPersister({storage: window.localStorage}).persisterFn
+    persister: experimental_createQueryPersister({storage: window.localStorage}).persisterFn
+    // persister: isServer
+    //   ? Promise.resolve
+    //   : experimental_createQueryPersister({storage: window.localStorage}).persisterFnpersister: isServer
   });
 
   const { mutateAsync: signOutUser, isPending: isSigningOut } = useMutation({
     mutationFn: () => authClient.signOut(),
-    onSuccess: async (res) => {
+    onSuccess: async () => {
       window.location.href = '/login';
     },
     retry: 0, // Disable retries for this mutation
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,5 @@
 import { Pool } from "pg";
 
-let dbConnected = false;
 const db = new Pool({
   host: import.meta.env.DATABASE_HOST,
   port: Number(import.meta.env.DATABASE_PORT),
@@ -13,18 +12,7 @@ const db = new Pool({
 });
 
 // Optional: Handle errors on idle clients
-db.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err);
-  // process.exit(-1); // Exit the process in case of a critical error
-});
-db.on('connect', ()=> {
-  console.log('connected..');
-  dbConnected = true;
-});
-
-const dbConnection = async () => {
-  if (!dbConnected) await db.connect();
-  return db;
-};
+db.on('error', (err) => console.error('Unexpected error on idle client', err));
+db.on('connect', ()=> console.log('connected..'));
 
 export default db;
