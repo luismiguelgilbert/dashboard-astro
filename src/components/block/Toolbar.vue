88058621505ba0013ff608847a782e872ab2a5e8
Luis Miguel Gilbert <luismiguelgilbert@gmail.com>
2025-10-15 01:24:37 -0500

    feat: version 0001

--- src/components/block/Toolbar.vue
+++ src/components/block/Toolbar.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
   <header v-if="props.position === 'top'" class="sticky top-16 z-40 h-10 w-full bg-white dark:bg-zinc-900 border-b dark:border-zinc-900">
     <slot />
   </header>
-  <footer v-if="props.position === 'bottom'" class="sticky bottom-0 z-40 h-10 w-full bg-white dark:bg-zinc-900 border-b dark:border-zinc-900">
+  <footer v-if="props.position === 'bottom'" class="sticky bottom-0 z-40 h-10 w-full bg-white dark:bg-zinc-900 border-t dark:border-zinc-900">
     <slot />
   </footer>
 </template>
